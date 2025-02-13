f34ea1b6145b67cf0fbc68df18e75d68a5a4a301
OmarMallouk <mr.mallouk.om@gmail.com>
2025-02-13 16:50:59 +0200

    fixed the summary issue

--- app/routes/timesheets.new/route.tsx
+++ app/routes/timesheets.new/route.tsx
@@ -12,21 +12,22 @@ import type { ActionFunction } from "react-router";
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const employee_id = formData.get("employee_id"); // <select /> input with name="employee_id"
+  const employee_id = formData.get("employee_id"); 
+  const work_summary = formData.get("work_summary");
   const start_time = formData.get("start_time");
   const end_time = formData.get("end_time");
 
   const db = await getDB();
   await db.run(
-    'INSERT INTO timesheets (employee_id, start_time, end_time) VALUES (?, ?, ?)',
-    [employee_id, start_time, end_time]
+    'INSERT INTO timesheets (employee_id, work_summary, start_time, end_time) VALUES (?,?, ?, ?)',
+    [employee_id, work_summary, start_time, end_time]
   );
 
   return redirect("/timesheets");
 }
 
 export default function NewTimesheetPage() {
-  const { employees } = useLoaderData(); // Used to create a select input
+  const { employees } = useLoaderData(); 
   return (
     <div>
       <h1>Create New Timesheet</h1>
@@ -43,6 +44,8 @@ export default function NewTimesheetPage() {
           ) )}
           </select>
         </div>
+        <label htmlFor="employee_id">Summary</label>
+        <input type="text" name="work_summary" id="work_summary" required  title="Enter a summary" />
         <div>
           <label htmlFor="start_time">Start Time</label>
           <input type="datetime-local" name="start_time" id="start_time" required />
